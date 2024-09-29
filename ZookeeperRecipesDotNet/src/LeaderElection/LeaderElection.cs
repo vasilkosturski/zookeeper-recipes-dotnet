a7b55a136a017ce97740bb97e88cbad0d0819806
vasilkosturski <v.kosturski.ext@sportradar.com>
2024-09-29 09:48:26 +0300

    Introduce a channel the serialize the check leadership calls

--- ZookeeperRecipesDotNet/src/LeaderElection/LeaderElection.cs
+++ ZookeeperRecipesDotNet/src/LeaderElection/LeaderElection.cs
@@ -1,16 +1,18 @@
 ﻿using org.apache.zookeeper;
-using Nito.AsyncEx;
+using System.Threading.Channels;
 
 namespace LeaderElection;
 
 public class LeaderElection : Watcher
 {
-    private readonly AsyncLock _mutex = new();
     private readonly string _zkConnectionString;
     private readonly string _electionPath;
     private readonly IElectionHandler _electionHandler;
     private ZooKeeper _zooKeeper;
     private string _znodePath;
+
+    // Channel to queue leadership tasks
+    private readonly Channel<Func<Task>> _leadershipTaskChannel = Channel.CreateUnbounded<Func<Task>>();
 
     public LeaderElection(string zkConnectionString, string electionPath, IElectionHandler electionHandler)
     {
@@ -18,6 +20,9 @@ public class LeaderElection : Watcher
         _electionPath = electionPath;
         _electionHandler = electionHandler;
         InitializeZooKeeper();
+
+        // Start the task processor to process leadership checks from the channel
+        Task.Run(ProcessLeadershipTasksAsync);
     }
 
     private void InitializeZooKeeper()
@@ -33,7 +38,8 @@ public class LeaderElection : Watcher
         _znodePath = await _zooKeeper.createAsync($"{_electionPath}/n_", new byte[0],
             ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 
-        await CheckLeadership();
+        // Enqueue the leadership check task in the channel
+        await _leadershipTaskChannel.Writer.WriteAsync(CheckLeadershipAsync);
     }
 
     private async Task EnsureElectionPathExistsAsync()
@@ -46,40 +52,48 @@ public class LeaderElection : Watcher
             }
             catch (KeeperException.NodeExistsException)
             {
+                // Path already exists, nothing to do here
             }
         }
     }
 
-    private async Task CheckLeadership()
+    // Channel processor that serializes leadership tasks
+    private async Task ProcessLeadershipTasksAsync()
     {
-        using (await _mutex.LockAsync())
+        await foreach (var leadershipTask in _leadershipTaskChannel.Reader.ReadAllAsync())
         {
-            var children = (await _zooKeeper.getChildrenAsync(_electionPath)).Children;
-            children.Sort();
+            await leadershipTask();
+        }
+    }
 
-            var currentNodeName = _znodePath.Substring(_znodePath.LastIndexOf('/') + 1);
+    // The leadership check logic enqueued into the channel
+    private async Task CheckLeadershipAsync()
+    {
+        var children = (await _zooKeeper.getChildrenAsync(_electionPath)).Children;
+        children.Sort();
 
-            // If the current node is the first in the list, it's the leader
-            var isLeader = currentNodeName == children[0]; 
-            if (isLeader)
+        var currentNodeName = _znodePath.Substring(_znodePath.LastIndexOf('/') + 1);
+
+        // If the current node is the first in the list, it's the leader
+        var isLeader = currentNodeName == children[0]; 
+        if (isLeader)
+        {
+            await _electionHandler.OnElectionComplete(true);
+        }
+        else
+        {
+            var index = children.IndexOf(currentNodeName);
+            if (index > 0)
             {
-                await _electionHandler.OnElectionComplete(true);
-            }
-            else
-            {
-                var index = children.IndexOf(currentNodeName);
-                if (index > 0)
-                {
-                    var previousNode = $"{_electionPath}/{children[index - 1]}";
+                var previousNode = $"{_electionPath}/{children[index - 1]}";
                     
-                    var stat = await _zooKeeper.existsAsync(previousNode, true);
+                var stat = await _zooKeeper.existsAsync(previousNode, true);
 
-                    // The node has gone missing between the call to getChildren() and exists().
-                    // We need to try and become the leader.
-                    if (stat == null)
-                    {
-                        await CheckLeadership();
-                    }
+                // The node has gone missing between the call to getChildren() and exists().
+                // We need to try and become the leader.
+                if (stat == null)
+                {
+                    await CheckLeadershipAsync();
                 }
             }
         }
@@ -89,7 +103,8 @@ public class LeaderElection : Watcher
     {
         if (@event.get_Type() == Event.EventType.NodeDeleted)
         {
-            await CheckLeadership();
+            // Enqueue a leadership check when a node is deleted
+            await _leadershipTaskChannel.Writer.WriteAsync(CheckLeadershipAsync);
         }
     }
 
